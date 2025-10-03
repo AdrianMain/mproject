027f27f34ad0440093df1a8c247c4b637220ef6b
AdrianMain <adrian.jakubek1234@gmail.com>
2025-10-03 20:31:09 +0200

    build 100%

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,45 @@
 <script lang="ts">
-let a = $state('');
-let b = $state('');
-let clicked = $state(false);
-function hello() {
+import { toDataURL } from "qrcode";
+
+let a = '';
+let b = '';
+let qrImg = '';
+let clicked = false;
+
+async function handleSubmit(event: Event) {
+    event.preventDefault();
     clicked = true;
-};
+    qrImg = await toDataURL(b);
+}
 </script>
 
 {#if !clicked}
-<h2>Witaj użytowniku Medicover</h2>
-<form action="" name="f">
-    <p>Podaj nazwę ośrodka np nazwa siłowni</p>
-    <input type="text" name="sport" autocomplete="on" bind:value={a}>
+<h2>Witaj użytkowniku Medicover</h2>
+<form on:submit={handleSubmit} autocomplete="on" name="medicoverForm">
+    <p>Podaj nazwę ośrodka (np. nazwa siłowni)</p>
+    <input
+        name="sport"
+        type="text"
+        autocomplete="on"
+        bind:value={a}
+        placeholder="Nazwa ośrodka"
+        required
+    >
+
     <p>Podaj kod Medicover</p>
-    <input type="text" name="code" autocomplete="on" bind:value={b}>
-    <input type="submit" onclick={hello}>
-    
+    <input
+        name="code"
+        type="text"
+        autocomplete="off"
+        bind:value={b}
+        placeholder="Kod Medicover"
+        required
+    >
+
+    <button type="submit">Wygeneruj</button>
 </form>
 {:else}
 <h1>{a}</h1>
 <p>{b}</p>
+<img alt="qr" src={qrImg} id="qrIMG">
 {/if}
-
-
-<style>
-</style>
