aabf0821494e03faca5b0b0cbe2e163ac5ec71ca
AdrianMain <adrian.jakubek1234@gmail.com>
2025-10-03 18:19:11 +0200

    basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,27 @@
 <script lang="ts">
-let ile = $state(0);
-function los() {
-    let a = Math.random();
-    a = Math.floor((Math.random() * 10)+1)
-    ile = a;
-}
+let a = $state('');
+let b = $state('');
+let clicked = $state(false);
+function hello() {
+    clicked = true;
+};
 </script>
 
-<nav>
-<p>Strona główna</p>
-</nav>
-<div class="main-container">
+{#if !clicked}
+<h2>Witaj użytowniku Medicover</h2>
+<form action="" name="f">
+    <p>Podaj nazwę ośrodka np nazwa siłowni</p>
+    <input type="text" name="sport" autocomplete="on" bind:value={a}>
+    <p>Podaj kod Medicover</p>
+    <input type="text" name="code" autocomplete="on" bind:value={b}>
+    <input type="submit" onclick={hello}>
+    
+</form>
+{:else}
+<h1>{a}</h1>
+<p>{b}</p>
+{/if}
 
-    <h1>Medicover</h1>
-    <table>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-            </tr>
-        </tbody>
-    </table>
-    <br>
-    <button onclick={los}>Losuj liczbę</button>
-    <p>{ile}</p>
-</div>
+
 <style>
-    table, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-        padding: 10px;
-    }
-    nav {
-        width: 100vw;
-        background-color: blue;
-        color: white;
-        height: 7vh;
-        overflow: hidden;
-    }
-    nav > p {
-        text-align: center;
-        font-family: 'Comic Sans MS';
-    }
-    :global(body) {
-        margin: 0px;
-    }
-    .main-container {
-        margin: 5px;
-    }
 </style>
